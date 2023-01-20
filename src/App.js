f31a59c2ed5e7b583ae5c3db29e1c671c576ffa7
GMohammadAli <gulammohammadali@gmail.com>
2023-01-21 00:18:21 +0530

    Metamask connected to hardhat

--- src/App.js
+++ src/App.js
@@ -13,14 +13,27 @@ import ETHDaddy from './abis/ETHDaddy.json'
 import config from './config.json';
 
 function App() {
+  const [account, setAccount] = useState(null)
+
+  const loadBlockchainData = async() => {
+    const accounts = await window.ethereum.request({ 'method':'eth_requestAccounts'})
+    const account = ethers.utils.getAddress(accounts[0])
+    setAccount(account)
+  }
+
+  useEffect(() => {
+    loadBlockchainData()
+  },[])
 
   return (
     <div>
 
       <div className='cards__section'>
-
+        <Navigation />
+        <Search />
+        <Domain />
         <h2 className='cards__title'>Welcome to ETH Daddy!</h2>
-
+        <p>{account}</p>
       </div>
 
     </div>
